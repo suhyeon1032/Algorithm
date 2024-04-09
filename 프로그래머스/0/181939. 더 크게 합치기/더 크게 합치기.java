8a81bc4418b9bc76bc9991e851e38ed1cdf571f1
daram <79134336+suhyeon1032@users.noreply.github.com>
2024-04-09 16:59:25 +0900

    [level 0] Title: 더 크게 합치기, Time: 9.75 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        String ab = "" + a + b;
+        String ba = "" + b + a;
+        
+        int answer_ab = Integer.parseInt(Integer.toString(a) + Integer.toString(b));
+        int answer_ba = Integer.parseInt(Integer.toString(b) + Integer.toString(a));
+        
+        if(answer_ab < answer_ba){
+            answer = answer_ba;
+        } else if(answer_ba < answer_ab){
+            answer = answer_ab;
+        } else answer = answer_ab;
+        
+        return answer;
+    }
+}

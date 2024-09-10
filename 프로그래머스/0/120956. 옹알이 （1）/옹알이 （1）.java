b6e2cf0cbc1e2fd55953a945e23a1249f274a3c4
daram <79134336+suhyeon1032@users.noreply.github.com>
2024-09-10 15:56:00 +0900

    [level 0] Title: 옹알이 (1), Time: 2.25 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String[] babbling) {
+        String[] arr = {"aya", "ye", "woo", "ma"};
+		
+		int answer = 0;
+		for(int i = 0; i < babbling.length; i++) {
+			for(int j = 0; j < arr.length; j++) {
+				if(babbling[i].contains(arr[j])) {
+					babbling[i] = babbling[i].replace(arr[j], " ");
+				}
+			}
+			if(babbling[i].trim().length() == 0) {
+				answer++;
+			}
+		}
+		
+		return answer;
+    }
+}
